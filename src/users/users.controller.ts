8ec0f29e9abaeaef44659abe3b47fc63d1066cc8
jogaec22 <jogaec22@gmail.com>
2022-07-06 15:57:25 -0500

    feat: improve exceptions

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
@@ -15,8 +15,12 @@ export class UsersController {
     }
 
     @Get('/:id')
-    findUser(@Param('id') id: string){
-        return this.userService.findOne(parseInt(id));
+    async findUser(@Param('id') id: string){
+        const user = await this.userService.findOne(parseInt(id));
+        if(!user){
+            throw new NotFoundException('User not found');
+        }
+        return user;
     }
 
     @Get()
