df762945af250f58fdab44d9ccc59ee109811240
jogaec22 <jogaec22@gmail.com>
2022-07-08 01:14:03 -0500

    feat: a bit of type safety around serializable

--- src/interceptors/serialize.interceptors.ts
+++ src/interceptors/serialize.interceptors.ts
@@ -2,17 +2,21 @@ import { CallHandler, ExecutionContext, NestInterceptor, UseInterceptors } from 
 import { plainToClass } from "class-transformer";
 import { map, Observable } from "rxjs";
 
-export function Serialize(dto: any){
+interface ClassConstructor {
+    new (...args: any[]): {};
+}
+
+export function Serialize(dto: ClassConstructor){
     return UseInterceptors(new SerializeInterceptor(dto));
 }
 
 export class SerializeInterceptor implements NestInterceptor {
 
-    constructor(private dto: any){}
+    constructor(private dto: ClassConstructor){}
 
     intercept(context: ExecutionContext, handler: CallHandler<any>): Observable<any> | Promise<Observable<any>> {        
         return handler.handle().pipe(
-            map((data: any) => {
+            map((data: ClassConstructor) => {
                 return plainToClass(this.dto, data, {
                     excludeExtraneousValues: true                 
                 });
