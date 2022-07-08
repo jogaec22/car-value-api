27128d53719a71962b1146815cb49979808bf585
jogaec22 <jogaec22@gmail.com>
2022-07-07 22:25:29 -0500

    feat: excluding response properties: serialization

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, ClassSerializerInterceptor, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query, UseInterceptors } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
@@ -15,6 +15,7 @@ export class UsersController {
     }
 
     @Get('/:id')
+    @UseInterceptors(ClassSerializerInterceptor)
     async findUser(@Param('id') id: string){
         const user = await this.userService.findOne(parseInt(id));
         if(!user){
