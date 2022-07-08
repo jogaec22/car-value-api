f3d56fe2b199425a826210d50f8e57fff61fafbd
jogaec22 <jogaec22@gmail.com>
2022-07-08 13:40:57 -0500

    feat: install packages & setting up about sessions

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+//import {CookieSession} from 'cookie-session';
+//import cookieSession from 'cookie-session';
+const cookieSession = require('cookie-session');
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieSession({
+    keys: ['afef3r']
+  }));
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true
